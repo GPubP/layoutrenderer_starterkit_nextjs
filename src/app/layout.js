1f49f27d5b5c968c9ce4647753a63f68c6230e4e
Nicolas T <nicolas@yapla.be>
2023-10-12 18:05:31 +0200

    📦 Install required packages / Add core branding CSS

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://cdn.antwerpen.be/core_branding_scss/6.5.0/main.min.css" />
+      </head>
       <body>{children}</body>
     </html>
   );
