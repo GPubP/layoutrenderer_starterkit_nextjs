0b1b5e872b63d5fab8ee8c7cd069181a10752973
Nicolas T <nicolas.teirlinckx@ucb.com>
2023-10-31 16:31:56 +0100

    Add explanation

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,13 @@ export default function Home() {
   return (
     <div style={{ padding: '100px' }}>
       <h1 className="u-margin-bottom">LayoutRenderer - NextJS Starterkit</h1>
+      <p className="u-margin-bottom">
+        Het eerste voorbeeld toont hoe de LayoutRenderer connectie kan leggen met een GraphQL store. Via die GraphQL
+        store wordt de titel van een website uit de redactie gehaald. Deze titel wordt vervolgens in een AlertWidget
+        gerenderd. Zet je ENV variabelen in de .env file om dit voorbeeld correct te laten werken. Het tweede voorbeeld
+        toont hoe je de layout kan definieren via LayoutProvider rules. Deze rules worden achtereenvolgens uitgevoerd
+        tot er 1 rule matcht.
+      </p>
       <ul>
         <li className="u-margin-bottom-xs">
           <a href="/redactie">Example 1 - LayoutRenderer met GraphQL & Redactie integratie voorbeeld</a>
